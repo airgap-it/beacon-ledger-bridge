c5b6e079f88305906ef1e7cb48991419835b6dcc
AndreasGassmann <andreas@andreasgassmann.ch>
2020-08-20 12:05:11 +0200

    feat(app): add support for message signing

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,5 @@
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
+const merge = require('webpack-merge')
+const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
   mode: 'development',
@@ -7,5 +7,5 @@ module.exports = merge(common, {
   devServer: {
     contentBase: './docs',
     https: true
-  },
-});
+  }
+})

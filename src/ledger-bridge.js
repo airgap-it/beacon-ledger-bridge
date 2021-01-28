0d14da6945cd8fdf4b1f6933eae38ed4d9959f5c
Andreas Gassmann <andreas@andreasgassmann.ch>
2021-01-28 20:06:00 +0000

    feat(sign): remove 03 prefix

--- src/ledger-bridge.js
+++ src/ledger-bridge.js
@@ -115,8 +115,8 @@ export default class BeaconLedgerBridge {
 
   async signOperation(operation, derivationPath = BeaconLedgerBridge.defaultDerivationPath) {
     const app = await this.createApp()
-    // "03" prefix because it's an operation: https://github.com/obsidiansystems/ledger-app-tezos/blob/master/src/apdu_sign.c#L582
-    const result = await app.signOperation(derivationPath, '03' + operation)
+    // Expect "03" or "05" prefix because it's an operation: https://github.com/obsidiansystems/ledger-app-tezos/blob/master/src/apdu_sign.c#L582
+    const result = await app.signOperation(derivationPath, operation)
     return result.signature
   }
 

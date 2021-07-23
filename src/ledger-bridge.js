2c2bf389f5d8da7adbb59fe6db36155440dad10e
acharl <amadeo.charle108@gmail.com>
2021-07-23 11:10:07 +0200

    feat(): use official Ledger Hardware Wallet Tezos JavaScript bindings

--- src/ledger-bridge.js
+++ src/ledger-bridge.js
@@ -1,7 +1,7 @@
 'use strict'
 
 import TransportWebUSB from '@ledgerhq/hw-transport-webusb'
-import Tezos from '@obsidiansystems/hw-app-xtz'
+import Tezos from '@ledgerhq/hw-app-tezos'
 
 const TARGET = 'BEACON-SDK-LEDGER-BRIDGE'
 
